f9e74532d807901e4da1e5c1295e656d8c5f489b
Parth Deshmukh <deshmukhparth293@gmail.com>
2024-05-21 00:02:22 +0530

    Changes

--- exp2.py
+++
@@ -1,6 +0,0 @@
-import cv2
-
-img=cv2.imread("sample.jpg")
-height,width,_=img.shape
-print(height)
-print(width)
--- exp3.py
+++
@@ -1,8 +0,0 @@
-import cv2
-interface = cv2.imread("interface/as2c.jpeg")
-interface = cv2.resize(interface, (1240, 680))
-bl = cv2.imread("interface/blackpage.png")
-bl = cv2.resize(bl, (1230 - 800, 670 - 10))
-interface[10:670, 800:1230] = bl
-interface = cv2.rectangle(interface, (127, 116), (640 + 127, 116 + 486), (0, 255, 50), 3)
-cv2.imwrite("newinterface.jpg",interface)
