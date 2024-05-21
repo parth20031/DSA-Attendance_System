33bca2736a910abc925785d451f44dd33f85f319
Parth Deshmukh <deshmukhparth293@gmail.com>
2024-05-21 10:03:14 +0530

    Bugs

--- setup.sh
+++ setup.sh
@@ -18,7 +18,7 @@ pip install virtualenv
 virtualenv venv
 
 # Activate the virtual environment (Windows)
-.\venv\Scripts\activate
+Source .\venv\Scripts\activate
 
 # Upgrade pip, setuptools, and wheel
 pip install --upgrade pip setuptools wheel
